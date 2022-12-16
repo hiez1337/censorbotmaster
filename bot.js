c10d442e255703b57d0223fec2db278e68a831a4
hiez1337 <44944483+hiez1337@users.noreply.github.com>
2022-12-16 13:46:14 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ let bot;
 
 if (isProduction) {
   bot = new Bot(token);
-  bot.setWebHook(process.env.HEROKU_URL + bot.token);
+  bot.setWebHook(process.env._URL + bot.token);
 } else {
   bot = new Bot(token, { polling: true });
 }
